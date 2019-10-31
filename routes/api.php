e255b44a9f3b7035f5427fd8a6e758f6a80bfd04
[mastermindlegion] <[martiin.chalupa@gmail.com]>
2019-10-31 16:32:56 +0100

    Pushed

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/person', 'Api\PersonController@index');
+
+Route::post('/login', 'Api\LoginController@login');
