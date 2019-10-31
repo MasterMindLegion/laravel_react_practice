17cfe0d77799d894822b442b08bf831097762774
Runi <aryuhan@gmail.com>
2019-10-31 13:37:51 +0100

    'hez'

---
+++ app/Http/Controllers/Api/PersonController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Person;
+
+class PersonController extends Controller
+{
+    public function index(){
+        $records = Person::limit(20)
+            ->orderBy('name','asc')
+            ->get();
+            dd($records);
+    } 
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+// use Symfony\Component\Routing\Annotation\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/person', 'Api\PersonController@index');
