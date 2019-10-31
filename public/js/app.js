220ddc7256edb60c7930ba4e62d8dde91b0463d7
laci <216ladislavhric@gmail.com>
2019-10-31 16:00:15 +0100

    random

--- public/js/app.js
+++ public/js/app.js
@@ -32068,7 +32068,7 @@ function (_React$Component) {
       loaded: false,
       data: []
     };
-    _this.url = 'http://www.mi6.test/api/person';
+    _this.url = 'http://www.mi6.test:8080/api/person';
     return _this;
   }
 
@@ -32122,7 +32122,7 @@ function (_React$Component) {
   return PeopleList;
 }(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
 /* render() {
-      let content = (
+     let content = (
          <div className="message">
             <div className="loader"><div></div><div></div><div></div><div></div></div>
             Loading
@@ -32130,36 +32130,35 @@ function (_React$Component) {
         <div>loading</div>
     )
     if (!this.state.loading && this.state.loaded) {
-              let list_items = [];
+         let list_items = [];
         for (let item of this.state.data) {
             list_items.push((
                 <li>
-                    { item.name }                            
+                    { item.name }
                     <div className="rating">{ item.rating.toFixed(1) }</div>
                 </li>
             ))
         }
-              content = (
+         content = (
             <>
                 <ul>
                     {
                         this.state.data.map((item) => (
                             <li>
-                                { item.name }                            
+                                { item.name }
                                 <div className="rating">{ item.rating.toFixed(1) }</div>
                             </li>
-                        )) 
+                        ))
                     }
                 </ul>
             </>
         )
     }
-    
-    return (
+     return (
         <section className="top-rated">
-                  <h2>Top rated movies & shows</h2>
-                  { content }
-              </section>
+             <h2>Top rated movies & shows</h2>
+             { content }
+         </section>
     );
 }
 */
@@ -32208,8 +32207,8 @@ react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\www\bootcamp\MI6_exercise\laravel-mi6-asset-db\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\www\bootcamp\MI6_exercise\laravel-mi6-asset-db\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/LACI/Web/laravel-mi6-asset-db/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/LACI/Web/laravel-mi6-asset-db/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
